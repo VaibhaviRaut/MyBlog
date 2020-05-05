2d9b9a68188757cedc58a920b461ba62b01f650a
Vaibhavi Raut <vaibhaviraut97@gmail.com>
2020-05-05 02:44:47 -0400

    creating new files

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 $('.parallax-window').parallax({imageSrc: '../img/unicorn1.jpg'});
+
+$('.profile').parallax({imageSrc: '../img/profile3.jpg'});
+
+$('.parallax-window-works').parallax({imageSrc: '../img/works1.jpg'});
+
+$('.img-text-wrapper').parallax({imageSrc: '../img/willowisp.jpg'});
+
+//$('.profile-subtitle').parallax({imageSrc: '../img/background2.jpg'});
+
+//$('.aboutme').getElementById("about").parallax({imageSrc: '../img/Profile.jpg'});
