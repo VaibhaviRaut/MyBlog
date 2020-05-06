c049be5d645ea2eb82b96fc34a15c0bfe22f2df2
Vaibhavi Raut <vaibhaviraut97@gmail.com>
2020-05-06 12:57:13 -0400

    creating new files

--- js/main.js
+++ js/main.js
@@ -2,9 +2,31 @@ $('.parallax-window').parallax({imageSrc: '../img/unicorn1.jpg'});
 
 $('.profile').parallax({imageSrc: '../img/profile3.jpg'});
 
-$('.parallax-window-works').parallax({imageSrc: '../img/works1.jpg'});
+$('.parallax-window-works').parallax({imageSrc: '../img/more3.jpg'});
 
-$('.img-text-wrapper').parallax({imageSrc: '../img/willowisp.jpg'});
+$('.img-text-wrapper').parallax({imageSrc: '../img/willowisp5.jpg'});
+
+
+const worksitems = document.querySelectorAll('.works-item-wrapper');
+worksitems.forEach(worksitem =>{
+  worksitem.addEventListener('mouseover', ()=>{
+    worksitem.childNodes[1].classList.add('img-darken');
+  })
+  worksitem.addEventListener('mouseout', ()=>{
+    worksitem.childNodes[1].classList.remove('img-darken')
+  })
+
+});
+
+
+
+
+
+
+
+//<div class="subtitle"><a class="WOTW" href="WOTW.html" data-parallax="scroll" data-z-index="3">Will o the Wisp</a></div>
+
+//$('.WOTW').parallax({imageSrc: '../img/willowisp.jpg'});
 
 //$('.profile-subtitle').parallax({imageSrc: '../img/background2.jpg'});
 
